4c3764f5957e66e672ac9f271a5e7503a1e45e90
ankit-gupta6791 <91ankitgupta@gmail.com>
2024-09-17 22:32:57 +0530

    Added code for news feed screen with pull to refresh

--- lib/screens/news_feed_screen.dart
+++ lib/screens/news_feed_screen.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import '../providers/news_provider.dart';
+import '../models/article.dart';
 
 class NewsFeedScreen extends StatefulWidget {
-  const NewsFeedScreen({super.key});
-
   @override
-  State<NewsFeedScreen> createState() => _NewsFeedScreenState();
+  _NewsFeedScreenState createState() => _NewsFeedScreenState();
 }
 
 class _NewsFeedScreenState extends State<NewsFeedScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        Provider.of<NewsProvider>(context, listen: false).fetchArticles();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Feed'),
+      ),
+      body: Consumer<NewsProvider>(
+        builder: (context, provider, child) {
+          if (provider.isLoading && provider.articles.isEmpty) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return RefreshIndicator(
+            onRefresh: () => provider.fetchArticles(isRefresh: true),
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: provider.articles.length + (provider.isLoading ? 1 : 0),
+              itemBuilder: (context, index) {
+                if (index == provider.articles.length && provider.isLoading) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                Article article = provider.articles[index];
+                return ListTile(
+                  title: Text(article.title),
+                  subtitle: Text(article.description),
+                  leading: article.urlToImage != null
+                      ? Image.network(article.urlToImage, width: 100)
+                      : Icon(Icons.image),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
